56f02db686ab6b072a007e99885ae833b33c1cb1
Manjara <mnjraj.2022@gmail.com>
2025-02-27 23:21:46 +0530

    Lesson - 02 HTTP(PT1)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
-import express from "express";
 import { PORT } from "./config.js";
+import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res
+    .status(234)
+    .send("<center><h1>Welcome to the Book Store!!</h1></center>");
+  // Using return to avoid further execution of any statement.
+});
+
 app.listen(PORT, () => {
-  console.log(`everything ok: ${PORT}`);
+  console.log("Working just fine");
 });
